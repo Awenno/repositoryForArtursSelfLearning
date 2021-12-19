0cbad050ed91cdb1030da0b4e4032a44a5626420
Artur Voronchuk <95566231+Awenno@users.noreply.github.com>
2021-12-19 22:11:34 +0300

    init commit

--- src/Lesson2_1/Shopping.java
+++ src/Lesson2_1/Shopping.java
@@ -9,47 +9,51 @@ public class Shopping {
 
         // массив продуктов:
         String[] products = {
-                "1.Молоко -",
-                "2.Хлеб -",
-                "3.Масло сливочное -",
-                "4.Сахар -",
-                "5.Творог(шайба) -"
+                "1.Молоко",
+                "2.Хлеб",
+                "3.Масло сливочное",
+                "4.Сахар",
+                "5.Творог(шайба)"
         };
 
         // массив стоимости продуктов:
         int[] cost = new int[]{55, 21, 170, 46, 99};
+        // массив количества выбранных продуктов:
+        int[] selectedProducts = new int[products.length];
+        // массив общей стоимости продуктов:
+        int[] unitPrice = new int[cost.length];
 
         System.out.println("\nСписок возможных товаров для покупки\n");
         for (int i = 0; i < products.length; i++) {
             System.out.println(products[i] + " " + cost[i] + " руб/шт");
         }
-
-        // массив количества выбранных продуктов
-        int[] selectedProduct = new int[products.length];
+        int productCount = 0;
+        int totalPrice = 0;
+        int productNumber = 0;
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.println("\nВыберите товар и количество или введите `end`");
             String inputStr = scanner.nextLine();
+
             if ("end".equals(inputStr)) {
                 System.out.println("Работа программы завершена.");
                 break;
             }
 
             String[] input = inputStr.split(" ");
-            int productNumber = Integer.parseInt(input[0]) - 1;
-            int productCount = Integer.parseInt(input[1]);
+            productNumber = Integer.parseInt(input[0]) - 1;
+            productCount = Integer.parseInt(input[1]);
 
-            selectedProduct[productNumber] = productCount;
+            unitPrice[productNumber] = cost[productNumber] * productCount;
+            selectedProducts[productNumber] = productCount;
 
-            int totalCost = 0;
-            for(int totalPrice : selectedProduct) {
-                totalCost += totalPrice;
+            for (int price : cost) {
+                totalPrice += price;
             }
-            System.out.println("Ваша корзина:\nНаименование товара Количество Цена/за.ед Общая стоимость");
-            System.out.println(productNumber + " " + productCount + " " + Arrays.toString(cost) + " " + totalCost);
-
         }
+        System.out.println("Ваша корзина:\nНаименование товара Количество Цена/за.ед Общая стоимость\n");
+        System.out.println(products[selectedProducts[productNumber]] + " " + productCount + " " + unitPrice[productNumber] + " " + totalPrice);
     }
 }
--- src/TestZone.java
+++ src/TestZone.java
@@ -2,8 +2,36 @@ import java.util.Scanner;
 
 public class TestZone {
 
-
     public static void main(String[] args) {
+        int[] temps = new int[31];
+        boolean[] filledDays = new boolean[31];
 
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введи день и температуру или end");
+            String input = scanner.nextLine(); // "13 -3" или "end"
+            if (input.equals("end")) {
+                break;
+            }
+            String[] parts = input.split(" "); // ["13", "-3"]
+            int inputDay = Integer.parseInt(parts[0]) - 1; // "13" -> 12
+            int inputTemp = Integer.parseInt(parts[1]); // "-3" -> -3
+
+            temps[inputDay] = inputTemp;
+            filledDays[inputDay] = true;
+
+            double sum = 0;
+            for (int t : temps) {
+                sum += t;
+            }
+            int cnt = 0;
+            for (boolean filled : filledDays) {
+                if (filled) {
+                    cnt++;
+                }
+            }
+            System.out.println("Средняя температура: " + (sum / cnt) + " на основе " + cnt + " дней");
+        }
     }
 }
