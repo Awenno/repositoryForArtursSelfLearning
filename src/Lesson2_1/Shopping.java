4019cc47fe2d338ea0598ac347ad4a83c7c91344
Awenno <fenix4you@mail.ru>
2021-12-21 15:36:31 +0300

    init

--- src/Lesson2_1/Shopping.java
+++ src/Lesson2_1/Shopping.java
@@ -1,6 +1,5 @@
 package Lesson2_1;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Shopping {
@@ -9,47 +8,53 @@ public class Shopping {
 
         // массив продуктов:
         String[] products = {
-                "1.Молоко -",
-                "2.Хлеб -",
-                "3.Масло сливочное -",
-                "4.Сахар -",
-                "5.Творог(шайба) -"
+                "1.Молоко",
+                "2.Хлеб",
+                "3.Масло",
+                "4.Сахар",
+                "5.Сыр"
         };
 
         // массив стоимости продуктов:
-        int[] cost = new int[]{55, 21, 170, 46, 99};
+        int[] cost = {55, 21, 170, 46, 99};
+        // массив количества выбранных продуктов:
+        int[] selectedProducts = new int[products.length];
+        // массив общей стоимости продуктов:
+        int[] selectedProductSum = new int[cost.length];
 
         System.out.println("\nСписок возможных товаров для покупки\n");
         for (int i = 0; i < products.length; i++) {
             System.out.println(products[i] + " " + cost[i] + " руб/шт");
         }
-
-        // массив количества выбранных продуктов
-        int[] selectedProduct = new int[products.length];
-
+        int productNumber = 0;
+        int productCount = 0;
+        int totalPrice = 0;
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.println("\nВыберите товар и количество или введите `end`");
             String inputStr = scanner.nextLine();
+
             if ("end".equals(inputStr)) {
-                System.out.println("Работа программы завершена.");
                 break;
             }
 
             String[] input = inputStr.split(" ");
-            int productNumber = Integer.parseInt(input[0]) - 1;
-            int productCount = Integer.parseInt(input[1]);
+            productNumber = Integer.parseInt(input[0]) - 1;
+            productCount = Integer.parseInt(input[1]);
 
-            selectedProduct[productNumber] = productCount;
-
-            int totalCost = 0;
-            for(int totalPrice : selectedProduct) {
-                totalCost += totalPrice;
-            }
-            System.out.println("Ваша корзина:\nНаименование товара Количество Цена/за.ед Общая стоимость");
-            System.out.println(productNumber + " " + productCount + " " + Arrays.toString(cost) + " " + totalCost);
-
+            selectedProductSum[productNumber] += cost[productNumber] * productCount;
+            selectedProducts[productNumber] += productCount;
         }
+        System.out.println("Ваша корзина:\nНаименование товара: Количество: Цена/за.ед: Общая стоимость:");
+        for (int i = 0; i < products.length; i++) {
+            // если i = 0 (товар не выбран), то НЕ выводить инфу о товаре на экран!
+            if (selectedProducts[i] != 0) {
+                System.out.println(products[i] + " " + selectedProducts[i] + " " + cost[i] + " " + selectedProductSum[i]);
+            }
+            totalPrice += selectedProductSum[i];
+        }
+        System.out.println("\nИтого: " + totalPrice + " рублей");
     }
 }
+
